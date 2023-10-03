6fd03e235dbc430fb42493ac26075360aa6dd934
Behrouz Babaeinezhad <behruz.pb@gmail.com>
2023-10-03 20:28:11 +0200

    add http module

--- src/events-manager/events-manager.module.ts
+++ src/events-manager/events-manager.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EventsManagerController } from './events-manager.controller';
-import { EventsManagerService } from './events-manager.service';
+import { EventsManagerService } from './EventsManagerService';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, HttpModule],
   controllers: [EventsManagerController],
   providers: [EventsManagerService],
 })
