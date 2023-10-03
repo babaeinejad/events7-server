ae74ac23cabc71aeb79db58d0fd7a54bbaaca57f
Behrouz Babaeinezhad <behruz.pb@gmail.com>
2023-10-03 17:39:30 +0200

    add event-manager dto

---
+++ src/events-manager/dtos/events-manager.dto.ts
@@ -0,0 +1,30 @@
+import { EvnetType } from '@prisma/client';
+import {
+  IsNotEmpty,
+  IsNumber,
+  Validate,
+  IsString,
+  IsEnum,
+} from 'class-validator';
+import { CustomRangeValidator } from '../validators/priority-validator';
+import { EventTypeValidator } from '../validators/event-type-validator';
+
+export class CreateEventDto {
+  @IsNotEmpty()
+  @IsNumber()
+  @Validate(CustomRangeValidator, [1, 10])
+  priority: number;
+
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsEnum(EvnetType)
+  @Validate(EventTypeValidator)
+  type: EvnetType;
+}
