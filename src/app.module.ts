3fe43995f42f9274965e9e35aad6a0dd7b4900cb
Behrouz Babaeinezhad <behruz.pb@gmail.com>
2023-10-03 17:38:22 +0200

    add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsManagerModule } from './events-manager/events-manager.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [EventsManagerModule],
+  imports: [EventsManagerModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
