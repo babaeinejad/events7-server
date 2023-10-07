82a1a1a8bbe27ea3cc70cf6922d363f0739d15b6
Behrouz Babaeinezhad <behruz.pb@gmail.com>
2023-10-07 13:31:06 +0200

    fix event manager controller test dependencies

--- src/events-manager/events-manager.controller.spec.ts
+++ src/events-manager/events-manager.controller.spec.ts
@@ -3,13 +3,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { EventsManagerController } from 'src/events-manager/events-manager.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { EventsManagerService } from './events-manager.service';
+import { ConfigModule } from '@nestjs/config';
 
 describe('EventsController', () => {
   let controller: EventsManagerController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [PrismaModule, HttpModule],
+      imports: [PrismaModule, HttpModule, ConfigModule],
       controllers: [EventsManagerController],
       providers: [EventsManagerService],
     }).compile();
